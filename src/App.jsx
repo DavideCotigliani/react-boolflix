7636d641ffd931b4ceefc0a9d95019d4b377c8c0
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-20 15:47:01 +0200

    [UPD] Milestone 2 completed with the second AJAX call for tv series

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [userDigit, setUserDigit] = useState("")
   // elenco dei film dell'api
   const [films, setFilms] = useState([]);
+  // elenco serie tv
+  const [tvSerie, setTvSerie] = useState([]);
 
   const languageToCountry = {
     en: "US",
@@ -37,13 +39,15 @@ function App() {
     e.preventDefault();
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a47554352f14abca2464ead192f2a073&query=${userDigit}`).then((resp) => {
       setFilms(resp.data.results);
+    });
+    axios.get(`https://api.themoviedb.org/3/search/tv?api_key=a47554352f14abca2464ead192f2a073&query=${userDigit}`).then((resp) => {
+      setTvSerie(resp.data.results)
     })
   };
 
-
   return (
     <>
-      <h1>Cerca film</h1>
+      <h1>Cerca film e serie TV</h1>
       <form onSubmit={handleEndpoint}>
         <input type="text" name="title" placeholder="Cerca film" value={userDigit} onChange={handleUserChange} />
         <button type="submit">Cerca</button>
@@ -56,6 +60,17 @@ function App() {
             <Flag code={getCountryCode(film.original_language)} />
             {film.original_language.toUpperCase()}</p>
           <p>Voto medio: {film.vote_average}</p>
+        </div>
+      ))}
+      {tvSerie.map((serie) => (
+        <div key={serie.id}>
+          <h3>{serie.name}</h3>
+          <p>Titolo originale: {serie.original_name}</p>
+          <p>Lingua originale:
+            <Flag code={getCountryCode(serie.original_language)} />
+            {serie.original_language.toUpperCase()}
+          </p>
+          <p>Voto medio: {serie.vote_average}</p>
         </div>
       ))}
     </>
